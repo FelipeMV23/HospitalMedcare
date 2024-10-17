5fef55abcd055800af2be83634fca7f59fe328fd
FelipeMV23 <136393326+FelipeMV23@users.noreply.github.com>
2024-10-17 20:12:15 -0300

    Update urls.py

--- Medcare/urls.py
+++ Medcare/urls.py
@@ -20,6 +20,7 @@ from Medcare import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
